681703b50a0919c2c30cc1b61834242dd70b2388
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-14 23:21:11 +0530

    Feat: Added basic express server setup with dotenv and nodemon

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const {PORT} = require('./config/server.config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text())
+
+app.get("/ping", (req, res) => {
+    return res.send("Pong!");
+})
+
+app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
+
