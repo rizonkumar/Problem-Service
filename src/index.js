60e13e2436f10581b75d43625dda8478868ebe2f
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-16 22:35:08 +0530

    Feat: Added Mongoose DB config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const errorHandler = require("./utils/errorHandler");
+const connectToDB = require("./config/db.config");
 
 const app = express();
 
@@ -11,16 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-// if any request comes and routes starts with /api, we map it to apiRouter
 app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "Problem Service is alive" });
 });
 
-// last middleware if any error comes
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started at PORT : ${PORT}`);
+  await connectToDB();
+  console.log("Successfully connected to DB");
 });
