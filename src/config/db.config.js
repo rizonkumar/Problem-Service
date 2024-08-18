e7ea86e2b3e1081b97f7b051648c3b42940d019b
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-18 07:31:50 +0530

    Feat: Implemented the logger for all the files

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 const { ATLAS_DB_URL, NODE_ENV } = require("./server.config");
+const logger = require("./logger.config");
 
 async function connectToDB() {
   try {
+    logger.info("Attempting to connect to MongoDB...");
     if (NODE_ENV === "development") {
       await mongoose.connect(ATLAS_DB_URL, {
         serverSelectionTimeoutMS: 30000,
         socketTimeoutMS: 45000,
       });
+      logger.info("Successfully connected to MongoDB");
     }
   } catch (error) {
-    console.error("Unable to connect to the DB server");
-    console.error("Error details:", error);
+    logger.error("Unable to connect to the DB server:", error);
+    throw error;
   }
 }
 
